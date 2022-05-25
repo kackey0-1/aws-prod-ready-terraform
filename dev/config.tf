b97810e881a34cf02d63884db60e99c6c9677295
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-05-25 18:24:04 +0900

    added dev resources

--- dev/config.tf
+++ dev/config.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
 terraform {
   backend "s3" {
-    bucket = "dev-env-terraform"
-    region = "ap-northeast-1"
-    key    = "dev-env-terraform.tfstate"
+    bucket  = "dev-env-terraform"
+    region  = "ap-northeast-1"
+    key     = "dev-env-terraform.tfstate"
+    encrypt = true
   }
 }
--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,3 @@
-resource "aws_instance" "web01" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
-
-  tags = {
-    name: "web01"
-  }
+module "dev" {
+  source = "../modules/"
 }
