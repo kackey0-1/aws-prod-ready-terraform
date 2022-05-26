2f393bb4b114fddd32783ff8d0df8bf764367593
kackey0-1 <50774322+kackey0-1@users.noreply.github.com>
2022-05-26 15:47:52 +0900

    destroy resources (#6)
    * destroy resources
    
    * destroy resources v2

--- dev/config.tf
+++ dev/config.tf
@@ -2,11 +2,3 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "dev-env-terraform"
-    region  = "ap-northeast-1"
-    key     = "dev-env-terraform.tfstate"
-    encrypt = true
-  }
-}
