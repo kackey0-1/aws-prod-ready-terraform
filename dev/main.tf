23f31f14f89a37f368dd0d33148092d02df8cca9
mona928 <70831583+mona928@users.noreply.github.com>
2022-05-29 17:21:57 +0900

    add iam policy (#8)
    * add iam policy
    
    * remove unnecesary iam policy
    
    * remove unnecesary iam policy_v2

---
+++ dev/main.tf
@@ -0,0 +1,16 @@
+module "describe_regions_for_ec2" {
+  source = "../modules/iam"
+  name = "describe_regions_for_ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
+
+
