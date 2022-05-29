00e38b72842eaa71671f7632b456112be372c40e
“maaana928” <chailatte.m928@gmail.com>
2022-05-29 16:59:41 +0900

    remove unnecesary iam policy

--- dev/main.tf
+++ dev/main.tf
@@ -13,29 +13,4 @@ data "aws_iam_policy_document" "allow_describe_regions" {
   }
 }
 
-resource "aws_iam_policy" "example" {
-  name = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
-
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["ec2.amazonaws.com"]
-      type = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example" {
-  policy_arn = aws_iam_policy.example.arn
-  role = aws_iam_role.example.name
-}
 
