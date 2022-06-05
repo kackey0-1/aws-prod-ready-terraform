cd4eb133e03f1c310850ae38d0aabad47d92c9db
“maaana928” <chailatte.m928@gmail.com>
2022-06-05 21:15:19 +0900

    resolve comment s3 bucket

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,19 @@
 module "aws_s3_bucket" {
-  source = "../modules/S3"
+  source = "../modules/s3"
   name = "aws_s3_bucket"
+}
+
+module "describe_regions_for_ec2" {
+  source = "../modules/iam"
+  name = "describe_regions_for_ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
 }
