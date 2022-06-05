f0811f436c0c1f225116b849b67884ef8465d715
“maaana928” <chailatte.m928@gmail.com>
2022-06-05 22:27:30 +0900

    resolve comment s3 bucket_v4

--- modules/s3/aws_s3_bucket.tf
+++ modules/s3/aws_s3_bucket.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket" "private" {
 
 resource "aws_s3_bucket_public_access_block" "private" {
   bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket" "public" {
