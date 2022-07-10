cc4092378c8ff579def5de1de82b51d978724492
“maaana928” <chailatte.m928@gmail.com>
2022-07-10 22:28:11 +0900

    make subnet output and CIDR block variable

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,6 +1,13 @@
+variable "vpc_cidr" {}
+variable "pub_subnet_cidr_0" {}
+variable "pub_subnet_cidr_1" {}
+variable "pri_subnet_cidr_0" {}
+variable "pri_subnet_cidr_1" {}
+
 #vpc
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  #cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
 
@@ -11,14 +18,16 @@ resource "aws_vpc" "example" {
 
 #public subnet
 resource "aws_subnet" "public_0" {
-  cidr_block = "10.0.1.0/24"
+  #cidr_block = "10.0.1.0/24"
+  cidr_block = var.pub_subnet_cidr_0
   vpc_id = aws_vpc.example.id
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
 }
 
 resource "aws_subnet" "public_1" {
-  cidr_block = "10.0.2.0/24"
+  #cidr_block = "10.0.2.0/24"
+  cidr_block = var.pub_subnet_cidr_1
   vpc_id = aws_vpc.example.id
   availability_zone = "ap-northeast-1c"
   map_public_ip_on_launch = true
@@ -52,14 +61,16 @@ resource "aws_route_table_association" "public_1" {
 
 #private subnet
 resource "aws_subnet" "private_0" {
-  cidr_block = "10.0.65.0/24"
+  #cidr_block = "10.0.65.0/24"
+  cidr_block = var.pri_subnet_cidr_0
   vpc_id = aws_vpc.example.id
   availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private_1" {
-  cidr_block = "10.0.66.0/24"
+  #cidr_block = "10.0.66.0/24"
+  cidr_block = var.pri_subnet_cidr_1
   vpc_id = aws_vpc.example.id
   availability_zone = "ap-northeast-1c"
   map_public_ip_on_launch = false
@@ -122,4 +133,13 @@ resource "aws_nat_gateway" "nat_gateway_1" {
 
 output "vpc_id" {
   value = aws_vpc.example.id
+}
+
+output "subnet_ids" {
+  value = {
+    pub_subnet_0 = aws_subnet.public_0.id
+    pub_subnet_1 = aws_subnet.public_1.id
+    pri_subnet_0 = aws_subnet.private_0.id
+    pri_subnet_1 = aws_subnet.private_1.id
+  }
 }
