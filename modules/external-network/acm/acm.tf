96ed674d690da0005db4732438d8d79bfc92a7a0
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-07-15 17:51:04 +0900

    fixed typo

--- modules/external-network/acm/acm.tf
+++ modules/external-network/acm/acm.tf
@@ -11,18 +11,18 @@ resource "aws_acm_certificate" "hypo-driven" {
 }
 
 # for inspection of certificate
-resource "aws_route53_record" "hypo-driven_certificate" {
-  name    = aws_acm_certificate.hypo-driven.domain_validation_options.resource_record_name
-  type    = aws_acm_certificate.hypo-driven.domain_validation_options.resource_record_type
-  records = [aws_acm_certificate.hypo-driven.domain_validation_options.resource_record_value]
-  zone_id = var.hypo-driven_aws_zone_id
-  ttl     = 60
-}
+#resource "aws_route53_record" "hypo-driven_certificate" {
+#  name    = aws_acm_certificate.hypo-driven.domain_validation_options[0].resource_record_name
+#  type    = aws_acm_certificate.hypo-driven.domain_validation_options[0].resource_record_type
+#  records = [aws_acm_certificate.hypo-driven.domain_validation_options[0].resource_record_value]
+#  zone_id = var.hypo-driven_aws_zone_id
+#  ttl     = 60
+#}
 
-resource "aws_acm_certificate_validation" "hypo-driven" {
-  certificate_arn         = aws_acm_certificate.hypo-driven.arn
-  validation_record_fqdns = [aws_route53_record.hypo-driven_certificate.fqdn]
-}
+#resource "aws_acm_certificate_validation" "hypo-driven" {
+#  certificate_arn         = aws_acm_certificate.hypo-driven.arn
+#  validation_record_fqdns = [aws_route53_record.hypo-driven_certificate.fqdn]
+#}
 
 output "aws_hypo-driven_acm_arn" {
   value = aws_acm_certificate.hypo-driven.arn
