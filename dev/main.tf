50e574dab5fdecaea475a55a87bc07cb50fdfb82
kackey0-1 <50774322+kackey0-1@users.noreply.github.com>
2022-07-17 21:45:47 +0900

    added kms feature (#26)

--- dev/main.tf
+++ dev/main.tf
@@ -58,17 +58,8 @@ module "aws_acm" {
   hypo-driven_aws_zone_id = module.aws_route53.hypo-driven_aws_zone_id
 }
 
-module "describe_regions_for_ec2" {
-  source     = "../modules/iam"
-  name       = "describe_regions_for_ec2"
-  identifier = "ec2.amazonaws.com"
-  policy     = data.aws_iam_policy_document.allow_describe_regions.json
-}
-
-data "aws_iam_policy_document" "allow_describe_regions" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
-    resources = ["*"]
-  }
+module "aws_kms" {
+  source      = "../modules/kms"
+  name        = "alias/example"
+  description = "Example Customer Master Key"
 }
