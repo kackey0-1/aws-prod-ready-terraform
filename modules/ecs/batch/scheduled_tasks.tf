c8db9e3e0738ca3ecaa3ca6278651f0303169387
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-07-18 13:51:14 +0900

    removed deprecated resources

--- modules/ecs/batch/scheduled_tasks.tf
+++ modules/ecs/batch/scheduled_tasks.tf
@@ -6,26 +6,26 @@ resource "aws_cloudwatch_log_group" "for_ecs_scheduled_task" {
 }
 
 resource "aws_ecs_task_definition" "example_batch" {
-  container_definitions = "./json/batch_container_definitions.json"
-  family                = "example-batch"
-  cpu                   = "256"
-  memory                = "516"
-  network_mode          = "awsvpc"
+  container_definitions    = "./json/batch_container_definitions.json"
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "516"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  ecs_execution_role_arn = module.ecs_batch_execution_role.iam_role_arn
+  ecs_execution_role_arn   = module.ecs_batch_execution_role.iam_role_arn
 }
 
 resource "aws_cloudwatch_event_rule" "example-batch" {
-  name = "example-batch"
-  description = "important batch process"
+  name                = "example-batch"
+  description         = "important batch process"
   schedule_expression = "cron(*/2 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "example-batch" {
   target_id = "example-batch"
-  rule  = aws_cloudwatch_event_rule.example-batch.name
-  role_arn = module.ecs_batch_event_role.iam_role_arn
-  arn = hypo-driven_ecs_cluster_arn
+  rule      = aws_cloudwatch_event_rule.example-batch.name
+  role_arn  = module.ecs_batch_event_role.iam_role_arn
+  arn       = hypo-driven_ecs_cluster_arn
 }
 
 # ---------------------
@@ -35,7 +35,7 @@ module "ecs_batch_execution_role" {
   source     = "../../iam"
   name       = "ecs-task-execution"
   identifier = "ecs-tasks.amazonaws.com"
-  policy = data.aws_iam_policy_document.ecs_task_execution.json
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
 
 module "ecs_batch_event_role" {
@@ -50,7 +50,7 @@ data "aws_iam_policy" "ecs_task_execution_role_policy" {
 }
 
 data "aws_iam_policy_document" "ecs_task_execution" {
-  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+  source_policy_documents = [data.aws_iam_policy.ecs_task_execution_role_policy.policy]
   statement {
     effect    = "Allow"
     actions   = ["ssm:GetParameters", "kms:Decrypt"]
