7eb1c1eaae09d49521c10c930ead91e9165d75e0
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-07-18 13:48:15 +0900

    fixed typo

--- modules/ecs/batch/scheduled_tasks.tf
+++ modules/ecs/batch/scheduled_tasks.tf
@@ -52,7 +52,7 @@ data "aws_iam_policy" "ecs_task_execution_role_policy" {
 data "aws_iam_policy_document" "ecs_task_execution" {
   source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
   statement {
-    effect    = "ALLOW"
+    effect    = "Allow"
     actions   = ["ssm:GetParameters", "kms:Decrypt"]
     resources = ["*"]
   }
