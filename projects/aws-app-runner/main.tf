a9fabafed3a7f007beb9e39f6880a1a598867272
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-07-30 14:43:01 +0900

    fixed policy setting

--- projects/aws-app-runner/main.tf
+++ projects/aws-app-runner/main.tf
@@ -25,14 +25,14 @@ module "apprunner_service_role" {
   source     = "../../modules/iam"
   name       = "${var.apprunner-service-role}AppRunnerECRAccessRole"
   identifier = "ecs-tasks.amazonaws.com"
-  policy     = data.aws_iam_policy_document.apprunner-service-assume-policy.json
+  policy     = aws_iam_policy.apprunner-policy.policy
 }
 
 module "apprunner_instance_role" {
   source     = "../../modules/iam"
   name       = "${var.apprunner-service-role}AppRunnerInstanceRole"
   identifier = "tasks.apprunner.amazonaws.com"
-  policy     = data.aws_iam_policy_document.apprunner-instance-assume-policy.json
+  policy     = data.aws_iam_policy_document.apprunner-instance-role-policy.json
 }
 
 data "aws_iam_policy_document" "apprunner-service-assume-policy" {
@@ -51,25 +51,9 @@ resource "aws_iam_role_policy_attachment" "apprunner-service-role-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
-resource "aws_iam_policy" "Apprunner-policy" {
-  name   = "Apprunner-getSSM"
+resource "aws_iam_policy" "apprunner-policy" {
+  name   = "apprunner-getSSM"
   policy = data.aws_iam_policy_document.apprunner-instance-role-policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "apprunner-instance-role-attachment" {
-  role       = module.apprunner_instance_role.iam_role_name
-  policy_arn = aws_iam_policy.Apprunner-policy.arn
-}
-
-data "aws_iam_policy_document" "apprunner-instance-assume-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["tasks.apprunner.amazonaws.com"]
-    }
-  }
 }
 
 data "aws_iam_policy_document" "apprunner-instance-role-policy" {
