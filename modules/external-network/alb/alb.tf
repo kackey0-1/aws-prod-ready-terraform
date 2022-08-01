78c3bbf934aae3f50327ede709ad2c2b513f8257
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-08-01 12:40:24 +0900

    fixed vpc_id variable

--- modules/external-network/alb/alb.tf
+++ modules/external-network/alb/alb.tf
@@ -2,7 +2,6 @@ variable "vpc_id" {}
 variable "public_subnets" {}
 variable "access_log_bucket_id" {}
 variable "aws_hypo-driven_acm_arn" {}
-variable "vpc_id" {}
 
 resource "aws_alb" "hypo-driven" {
   name                             = "hypo-driven"
@@ -30,28 +29,28 @@ resource "aws_alb" "hypo-driven" {
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_alb.hypo-driven.arn
-  port = 443
-  protocol = "HTTPS"
-  certificate_arn = var.aws_hypo-driven_acm_arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = var.aws_hypo-driven_acm_arn
   default_action {
     type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
       message_body = "this is HTTPS response"
-      status_code = "200"
+      status_code  = "200"
     }
   }
 }
 
 resource "aws_alb_listener" "redirect_http_to_https" {
   load_balancer_arn = aws_alb.hypo-driven.arn
-  port = 8080
-  protocol = "HTTP"
+  port              = 8080
+  protocol          = "HTTP"
   default_action {
     type = "redirect"
     redirect {
-      port = 443
-      protocol = "HTTPS"
+      port        = 443
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
   }
@@ -82,22 +81,22 @@ module "http_redirect_sg" {
 }
 
 resource "aws_lb_target_group" "target_to_ecs" {
-  name = "hypo-driven"
-  target_type = "ip"
-  vpc_id = var.vpc_id
-  port = 80
-  protocol = "HTTP"
+  name                 = "hypo-driven"
+  target_type          = "ip"
+  vpc_id               = var.vpc_id
+  port                 = 80
+  protocol             = "HTTP"
   deregistration_delay = 300
 
   health_check {
-    path = "/"
-    healthy_threshold = 5
+    path                = "/"
+    healthy_threshold   = 5
     unhealthy_threshold = 2
-    timeout = 5
-    interval = 30
-    matcher = 200
-    port = "traffic-port"
-    protocol = "HTTP"
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
   }
 
   depends_on = [aws_alb.hypo-driven]
@@ -105,10 +104,10 @@ resource "aws_lb_target_group" "target_to_ecs" {
 
 resource "aws_lb_listener_rule" "hypo-driven" {
   listener_arn = aws_lb_listener.https.arn
-  priority = 100
+  priority     = 100
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.target_to_ecs.arn
   }
 
@@ -121,8 +120,8 @@ resource "aws_lb_listener_rule" "hypo-driven" {
 
 output "hypo-driven_alb" {
   value = {
-    hypo-driven_alb_name    = aws_alb.hypo-driven.dns_name
-    hypo-driven_alb_zone_id = aws_alb.hypo-driven.zone_id
+    hypo-driven_alb_name             = aws_alb.hypo-driven.dns_name
+    hypo-driven_alb_zone_id          = aws_alb.hypo-driven.zone_id
     hypo-driven_alb_target_group_arn = aws_lb_target_group.target_to_ecs.arn
   }
 }
